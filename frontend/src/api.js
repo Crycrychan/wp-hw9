8b921c523ff04950cc216330bd1ace854e5701e4
Andy Chou <andy9269@gmail.com>
2022-12-29 15:33:04 +0800

    fixed api.js again

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_ROOT =
   process.env.NODE_ENV === "production"
     ? "/api"
     : "http://localhost:4000/api";
 
-const WS_URL =
-  process.env.NODE_ENV === "production"
-    ? window.location.origin.replace(/^https*/, "ws")
-    : "ws://localhost:4000";
+const instance = axios.create({
+  baseURL: API_ROOT,
+});
 
-export const api = axios.create({ baseURL: API_ROOT });
-export const ws = new WebSocket(WS_URL);
+export default instance;
+
+// instance.get('/hi').then((data) => console.log(data));
