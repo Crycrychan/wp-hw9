d0305f63091eb8142fefaa952e036e5e3bbf9fa6
Andy Chou <andy9269@gmail.com>
2022-12-29 17:05:49 +0800

    so i will try one last fucking time, damnn it

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,6 @@ const port = process.env.PORT || 4000;
 app.listen(port, () =>
  console.log(`Example app listening on port ${port}!`),
 );
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
