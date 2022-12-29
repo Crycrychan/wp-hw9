616574ba70074ab38eb31d5400ca8cc162093320
Andy Chou <andy9269@gmail.com>
2022-12-29 15:26:50 +0800

    modified api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
-});
+const API_ROOT =
+  process.env.NODE_ENV === "production"
+    ? "/api"
+    : "http://localhost:4000/api";
 
-export default instance;
+const WS_URL =
+  process.env.NODE_ENV === "production"
+    ? window.location.origin.replace(/^https*/, "ws")
+    : "ws://localhost:4000";
 
-// instance.get('/hi').then((data) => console.log(data));
+export const api = axios.create({ baseURL: API_ROOT });
+export const ws = new WebSocket(WS_URL);
